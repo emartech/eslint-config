8e89fd2d8a955d9491aae06ae0890715c500b15c
István Szénási <szeist@gmail.com>
2016-10-07 12:47:19 +0200

    fix(security): remove unnecessary security rules

--- index.js
+++ index.js
@@ -52,10 +52,8 @@ module.exports = {
     'security/detect-buffer-noassert': 1,
     'security/detect-child-process': 1,
     'security/detect-eval-with-expression': 1,
-    'security/detect-non-literal-fs-filename': 1,
     'security/detect-non-literal-regexp': 1,
     'security/detect-non-literal-require': 1,
-    'security/detect-object-injection': 1,
     'security/detect-possible-timing-attacks': 1,
     'security/detect-pseudoRandomBytes': 1,
     'no-only-tests/no-only-tests': 2
